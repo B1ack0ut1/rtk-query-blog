a493a8940e3ea1505297c093fcd153f0442568a9
B1ack0ut1 <42845068+B1ack0ut1@users.noreply.github.com>
2024-04-06 01:27:01 +0800

    Extended api slice to postsSlice and also implemented optimistic update for reaction buttons

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { extendedApiSlice } from "./features/posts/postsSlice.js";
+import { fetchUsers } from "./features/users/usersSlice";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+store.dispatch(extendedApiSlice.endpoints.getPosts.initiate());
+store.dispatch(fetchUsers());
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
+  </React.StrictMode>
+);
